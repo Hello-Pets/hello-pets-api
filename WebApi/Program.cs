b7a9ff359638ab243f15a6ee37c13299fca2c1a1
oGrandeK <kelvimkauam@gmail.com>
2024-04-07 12:00:02 -0300

    first commit

---
+++ WebApi/Program.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
