c31bf1cd230f74a719de682304fef13729766918
Andershowrj <anderson92martins@gmail.com>
2024-05-03 23:05:28 +0100

    Criado entitade Tutor, criado exception para email e craido VO Email

---
+++ Data/Entities/Tutor.cs
@@ -0,0 +1,12 @@
+﻿using HelloPets.Data.Entities;
+using HelloPets.Data.ValueObjects;
+
+namespace Data.Entities
+{
+    internal class Tutor : Entity
+    {
+        public Name Name { get; } = null!;
+
+        
+    }
+}
---
+++ Data/ValueObjects/Email.cs
@@ -0,0 +1,39 @@
+﻿using Data.Exceptions;
+using HelloPets.Data.ValueObjects;
+using System.Text.RegularExpressions;
+
+namespace Data.ValueObjects
+{
+    internal class Email : ValueObject
+    {
+        public string Address { get; private set; } = null!;
+        //TODO: implantar verification
+
+
+        private Email() { }
+
+        const string Pattern = @"\w+@\w+\.\w+";
+
+        private static Regex EmailRegex = new Regex(Pattern);
+
+        public Email(string address)
+        {
+            Address = address;
+        }
+
+        private static void Validate(string adress)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(adress.Trim()), "Email cannot be empty");
+            DomainExceptionValidation.When(adress.Trim().Length < 5 || adress.Trim().Length > 100,
+                "Email must have between 5 and 100 characters");
+            DomainExceptionValidation.When(!EmailRegex.IsMatch(adress.Trim()), "Invalid email");
+        }
+
+        public static implicit operator Email(string adress) => new Email(adress);
+
+        public static implicit operator string(Email email) => email.ToString();
+
+        public override string ToString() => Address;
+
+    }
+}
