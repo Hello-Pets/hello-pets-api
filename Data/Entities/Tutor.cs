3b8575268b2d8ace9763cfeb0e83fbc39f514e85
Andershowrj <anderson92martins@gmail.com>
2024-05-06 22:02:14 +0100

    Feita estrutura da entidade Tutor apenas com nome e email, faltam outros atributos

--- Data/Entities/Tutor.cs
+++ Data/Entities/Tutor.cs
@@ -1,4 +1,5 @@
-﻿using HelloPets.Data.Entities;
+﻿using Data.ValueObjects;
+using HelloPets.Data.Entities;
 using HelloPets.Data.ValueObjects;
 
 namespace Data.Entities
@@ -6,7 +7,22 @@ namespace Data.Entities
     internal class Tutor : Entity
     {
         public Name Name { get; } = null!;
-
+        public Email Email { get; } = null!;
+        //TODO Implementar atributos faltantes
         
+        private Tutor() { }
+
+        public Tutor(Name name, Email email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public static implicit operator string(Tutor tutor) => tutor.ToString();
+
+        public override string ToString()
+        {
+            return $"{Name} {Email}";
+        }
     }
 }
