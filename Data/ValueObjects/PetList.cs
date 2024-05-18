c33911b0746ca40936a316f9215a2672f0dec253
Andershowrj <anderson92martins@gmail.com>
2024-05-18 20:22:23 +0100

    Implementado PetList (nao terminado)

---
+++ Data/ValueObjects/PetList.cs
@@ -0,0 +1,24 @@
+﻿using HelloPets.Data.Entities;
+using HelloPets.Data.ValueObjects;
+
+namespace Data.ValueObjects
+{
+    internal class PetList : ValueObject//, IEquatable<PetList>
+    {
+        public List<Pet> Pet { get; private set; } = null!;
+
+        private PetList() { }
+
+        public PetList(List<Pet> pets)
+        {
+            Pet = pets ?? new List<Pet>();
+        }
+
+        //Como usar o ToString aqui
+        public override string ToString() => $"";
+        //implementar Equals
+        public override int GetHashCode() => Pet.GetHashCode();
+    }
+
+    
+}
