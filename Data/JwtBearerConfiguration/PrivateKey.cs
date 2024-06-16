db6c986e7735d0b137144f71e99bead6c734f47a
Andershowrj <anderson92martins@gmail.com>
2024-06-16 18:45:35 +0100

    Criado private key temporaria

---
+++ Data/JwtBearerConfiguration/PrivateKey.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.JwtBearerConfiguration
+{
+    //Esta classe deve ser apagada antes da producao e com uma nova secret key armazenada com seguranca
+    public static class PrivateKey
+    {
+        public static string Key { get; set; } = "W-rAG{CU5ZfFEuGsm5$fzYmM7-f,j>";
+    }
+}
