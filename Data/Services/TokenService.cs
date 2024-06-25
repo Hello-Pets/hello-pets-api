de3502be95cf11fcddc1a5508bf3101639edcee4
Andershowrj <anderson92martins@gmail.com>
2024-06-25 21:48:45 +0100

    refatorado TokenService com novos claims e novo metodo para facilitar a legibilidade e manutencao

--- Data/Services/TokenService.cs
+++ Data/Services/TokenService.cs
@@ -16,16 +16,35 @@ namespace Data.Services
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim("id", tutor.Id.ToString()) }),
+                Subject = new ClaimsIdentity(GetClaims(tutor))/*Claims separado em um novo metodo*/,
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
                 SecurityAlgorithms.HmacSha256Signature),
                 Expires = DateTime.UtcNow.AddHours(12)
             };
 
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
+            try
+            {
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error generating token", ex);
+            }
+        }
+        private static IEnumerable<Claim> GetClaims(Tutor tutor)
+        {
+            return
+            [
+                new Claim("id", tutor.Id.ToString()),
+                new Claim("nome", tutor.Name),
+                new Claim("email", tutor.Email),
+                //Insere a data de criacao do token ao token
+                new Claim(JwtRegisteredClaimNames.Iat,
+                    DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+                //Cria um Id unico para o token dando maior seguranca
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            ];
         }
     }
 }
