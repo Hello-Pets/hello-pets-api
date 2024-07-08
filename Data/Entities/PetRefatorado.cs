4f529d26ff5c995b5a7f227e9606b6e439356584
Andershowrj <anderson92martins@gmail.com>
2024-07-08 23:39:31 +0100

    Adicionado Breed faltante e listas faltantes

--- Data/Entities/PetRefatorado.cs
+++ Data/Entities/PetRefatorado.cs
@@ -20,12 +20,17 @@ namespace Data.Entities
         public bool Neutered { get; private set; }
         public bool HasMicroChip { get; private set; }
         public int ProfileImageId { get; private set; }
-        public int BredddId { get; private set; }
         public string Size { get; private set; }
+        public int BredddId { get; private set; }
+        public Breed Breed { get; private set; }
+        public virtual List<TutorRefatorado> Tutors { get; private set; }
+        public virtual List<Trait> Traits { get; private set; }
+        public virtual List<Preference> Preferences { get; private set; }
+        public virtual List<SprecialNeeds> SprecialNeeds { get; private set; }
 
         private PetRefatorado() { }
 
-        public PetRefatorado(int id, string name, string document, string documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string nickname, string furcolor, string bio, DateTime birthdate, bool neutered, bool hasMicroChip, int profileImageId, int bredddId, string size) : base(id,  name, document, documentType, createdAt, updatedAt, isActive, publicId, bio, birthdate, profileImageId)
+        public PetRefatorado(int id, string name, string document, string documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string nickname, string furcolor, string bio, DateTime? birthdate, bool neutered, bool hasMicroChip, int profileImageId, string size, int bredddId, Breed breed, List<TutorRefatorado> tutors, List<Trait> traits, List<Preference> preferences, List<SprecialNeeds> sprecialNeeds) : base(id, name, document, documentType, createdAt, updatedAt, isActive, publicId, bio, birthdate, profileImageId)
         {
             Id = id;
             Name = name;
@@ -42,8 +47,13 @@ namespace Data.Entities
             Neutered = neutered;
             HasMicroChip = hasMicroChip;
             ProfileImageId = profileImageId;
-            BredddId = bredddId;
             Size = size;
+            BredddId = bredddId;
+            Breed = breed;
+            Tutors = tutors;
+            Traits = traits;
+            Preferences = preferences;
+            SprecialNeeds = sprecialNeeds;
         }
     }
 }
