9d277f7c880732e0be72da271c27c985e4e602aa
Andershowrj <anderson92martins@gmail.com>
2024-07-08 23:54:02 +0100

    Criada classe File (duvidas nela)

---
+++ Data/ValueObjects/File.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.ValueObjects
+{
+    public class File
+    {
+        //Como sera a referencia desta classe em tutor?
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        //Duvida aqui
+        public FileType Type { get; private set; }
+        public string PublicLink { get; set; }
+    }
+}
