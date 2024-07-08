040e7fe9622ff045dd491fb33fddc8e51d1bd25d
Andershowrj <anderson92martins@gmail.com>
2024-07-09 00:03:44 +0100

    Criada classe SpecialNeeds

---
+++ Data/ValueObjects/SpecialNeeds.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.ValueObjects
+{
+    public class SpecialNeeds
+    {
+        public int Id { get; set; }
+        public int PetId { get; set; }
+        public string Value { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
