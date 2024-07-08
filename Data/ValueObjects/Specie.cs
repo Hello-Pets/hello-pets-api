805a13d7ac542747aae4877dae4cbd0f6a89cbda
Andershowrj <anderson92martins@gmail.com>
2024-07-08 23:49:09 +0100

    Refatorada classe Specie

--- Data/ValueObjects/Specie.cs
+++ Data/ValueObjects/Specie.cs
@@ -1,38 +1,9 @@
-using System.Text.Json.Serialization;
-using HelloPets.Data.Enums;
-
-namespace HelloPets.Data.ValueObjects;
-
-public class Specie : ValueObject, IEquatable<Specie>
+﻿namespace Data.ValueObjects
 {
-    public SpecieEnum PetSpecie { get; private set; }
-    public string Breed { get; private set; } = null!;
-
-    [JsonConstructor]
-    public Specie(int petSpecie = 0, string breed = "Other")
+    public class Specie
     {
-        Validate(petSpecie, breed.Trim());
-
-        PetSpecie = (SpecieEnum)petSpecie;
-        Breed = breed.Trim();
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsActive { get; private set; }
     }
-
-    private Specie() {}
-
-    private void Validate(int petSpecie, string breed)
-    {
-        if(!Enum.IsDefined(typeof(SpecieEnum), petSpecie)) throw new ArgumentException("Invalid pet specie");
-
-        if(breed.Length < 3 || breed.Length > 20) throw new ArgumentException("Breed must be between 3 and 20 characters");
-
-        if(breed.Any(ch => char.IsDigit(ch))) throw new ArgumentException("Breed cannot contain digit");
-
-        if(breed.Any(ch => char.IsPunctuation(ch))) throw new ArgumentException("Breed cannot contain pontuaction");
-    }
-
-    public override string ToString() => Breed;
-
-    public override int GetHashCode() => Breed.GetHashCode();
-
-    public bool Equals(Specie? specie) => Breed == specie?.Breed;
 }
