45e1e0a9895bd73e61fb71e8143adb7891bd3693
Andershowrj <anderson92martins@gmail.com>
2024-07-08 23:19:18 +0100

    corrigido int em Id e removido ToString incorreto em "protected Entity"

--- Data/Entities/Entity.cs
+++ Data/Entities/Entity.cs
@@ -2,7 +2,7 @@ namespace HelloPets.Data.Entities;
 
 public abstract class Entity
 {
-    public string Id {get; private set;}
+    public int Id {get; private set;}
     public string Name { get; private set;}
     public string Document { get; private set; }
     public string DocumentType { get; private set; }
@@ -13,8 +13,9 @@ public abstract class Entity
     public string Bio { get; private set; }
     public DateTime Birthdate { get; private set; }
     public int ProfileImageId { get; private set; }
-    protected Entity() => Id = Guid.NewGuid().ToString()[0..5].ToUpper();
 
-    public bool Equals(string id) => Id == id;
+    protected Entity() => PublicId = Guid.NewGuid();
+
+    public bool Equals(int id) => Id == id;
     public override int GetHashCode() => Id.GetHashCode();
 }
