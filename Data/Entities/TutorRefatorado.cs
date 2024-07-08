d7c278f50dfb7adcb04ffc4c0b6827ccdfd15dc5
Andershowrj <anderson92martins@gmail.com>
2024-07-08 22:06:29 +0100

    Criada classe "TutorRefatorado"

---
+++ Data/Entities/TutorRefatorado.cs
@@ -0,0 +1,46 @@
+﻿namespace Data.Entities
+{
+    internal class TutorRefatorado
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string DocumentType { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+        public bool IsActive { get; private set; }
+        public Guid PublicId { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Salt { get; private set; }
+        public string Bio { get; private set; }
+        public DateTime Birthdate { get; private set; }
+        public string Phone { get; private set; }
+        public string Address { get; private set; }
+        public int ProfileImageId { get; private set; }
+        public string UserType { get; private set; }
+
+        private TutorRefatorado() { }
+
+        public TutorRefatorado(int id, string name, string document, string documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string username, string password, string salt, string bio, DateTime birthdate, string phone, string address, int profileImageId, string userType)
+        {
+            Id = id;
+            Name = name;
+            Document = document;
+            DocumentType = documentType;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            IsActive = isActive;
+            PublicId = publicId;
+            Username = username;
+            Password = password;
+            Salt = salt;
+            Bio = bio;
+            Birthdate = birthdate;
+            Phone = phone;
+            Address = address;
+            ProfileImageId = profileImageId;
+            UserType = userType;
+        }
+    }
+}
