12e6f5febbaa00da1b2344920821a500506254a8
Andershowrj <anderson92martins@gmail.com>
2024-07-08 23:57:33 +0100

    Criada classe Trait

---
+++ Data/ValueObjects/Trait.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.ValueObjects
+{
+    public class Trait
+    {
+        public int Id { get; private set; }
+        public string Traits { get; private set; }
+        public bool IsActive { get; private set; }
+    }
+}
