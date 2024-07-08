d61432be7b5df83751e46a257c1c6c7e373c9a92
Andershowrj <anderson92martins@gmail.com>
2024-07-08 22:49:21 +0100

    Refatorada classe base entity com as props comuns

--- Data/Entities/Entity.cs
+++ Data/Entities/Entity.cs
@@ -3,6 +3,16 @@ namespace HelloPets.Data.Entities;
 public abstract class Entity
 {
     public string Id {get; private set;}
+    public string Name { get; private set;}
+    public string Document { get; private set; }
+    public string DocumentType { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public bool IsActive { get; private set; }
+    public Guid PublicId { get; private set; }
+    public string Bio { get; private set; }
+    public DateTime Birthdate { get; private set; }
+    public int ProfileImageId { get; private set; }
     protected Entity() => Id = Guid.NewGuid().ToString()[0..5].ToUpper();
 
     public bool Equals(string id) => Id == id;
