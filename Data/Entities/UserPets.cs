30d6e155934d532cdf12d207866b491f37d6d16d
Andershowrj <anderson92martins@gmail.com>
2024-07-09 00:41:13 +0100

    Criada classe UserPets

---
+++ Data/Entities/UserPets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class UserPets
+    {
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public int PetId { get; private set; }
+        public bool IsActive { get; private set; }
+    }
+}
