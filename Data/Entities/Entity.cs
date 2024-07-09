c7092f1b1969f730f7b4af3e738ee6adb466bb5f
Andershowrj <anderson92martins@gmail.com>
2024-07-09 22:23:01 +0100

    Adicionada prop FileType

--- Data/Entities/Entity.cs
+++ Data/Entities/Entity.cs
@@ -1,3 +1,5 @@
+using Data.Enums;
+
 namespace HelloPets.Data.Entities;
 
 public abstract class Entity
@@ -5,17 +7,17 @@ public abstract class Entity
     public int Id {get; private set;}
     public string Name { get; private set;}
     public string Document { get; private set; }
-    public string DocumentType { get; private set; }
+    public DocumentType DocumentType { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime? UpdatedAt { get; private set; }
     public bool IsActive { get; private set; }
     public Guid PublicId { get; private set; }
     public string Bio { get; private set; }
     public DateTime? Birthdate { get; private set; }
-    public int ProfileImageId { get; private set; }
+    public FileType ProfileImageId { get; private set; }
     public virtual HelloPetsFile File { get; private set; }
 
-    protected Entity(int id, string name, string document, string documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string bio, DateTime birthdate, int profileImageId, HelloPetsFile file)
+    protected Entity(int id, string name, string document, DocumentType documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string bio, DateTime birthdate, FileType profileImageId, HelloPetsFile file)
     {
         Id = id;
         Name = name;
