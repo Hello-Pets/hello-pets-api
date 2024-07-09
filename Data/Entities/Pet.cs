845f4cf9c1d9ba0d7508e6806008f49f9f8c6706
Andershowrj <anderson92martins@gmail.com>
2024-07-09 22:19:05 +0100

    Trocado string size para size size

--- Data/Entities/Pet.cs
+++ Data/Entities/Pet.cs
@@ -1,4 +1,6 @@
-﻿namespace HelloPets.Data.Entities
+﻿using Data.Enums;
+
+namespace HelloPets.Data.Entities
 {
     public class Pet : Entity
     {
@@ -6,7 +8,7 @@
         public string Furcolor { get; private set; }
         public bool Neutered { get; private set; }
         public bool HasMicroChip { get; private set; }
-        public string Size { get; private set; }
+        public Size Size { get; private set; }
         public int BreedId { get; private set; }
         public virtual Breed Breed { get; private set; }
         public virtual ICollection<Tutor> Tutors { get; private set; }
@@ -17,7 +19,7 @@
 
         private Pet() { }
 
-        public Pet(int id, string name, string document, string documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string nickname, string furcolor, string bio, DateTime birthdate, bool neutered, bool hasMicroChip, int profileImageId, string size, int breedId, Breed breed, List<Tutor> tutors, List<Trait> traits, List<Preference> preferences, List<SpecialNeeds> specialNeeds, ICollection<UserPets> userPets, HelloPetsFile file) : base(id, name, document, documentType, createdAt, updatedAt, isActive, publicId, bio, birthdate, profileImageId, file)
+        public Pet(int id, string name, string document, string documentType, DateTime createdAt, DateTime updatedAt, bool isActive, Guid publicId, string nickname, string furcolor, string bio, DateTime birthdate, bool neutered, bool hasMicroChip, int profileImageId, Size size, int breedId, Breed breed, List<Tutor> tutors, List<Trait> traits, List<Preference> preferences, List<SpecialNeeds> specialNeeds, ICollection<UserPets> userPets, HelloPetsFile file) : base(id, name, document, documentType, createdAt, updatedAt, isActive, publicId, bio, birthdate, profileImageId, file)
         {
             Nickname = nickname;
             Furcolor = furcolor;
