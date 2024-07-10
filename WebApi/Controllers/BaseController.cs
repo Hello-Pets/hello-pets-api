58767fad413b2785f20525a21ffa842bda883373
oGrandeK <kelvimkauam@gmail.com>
2024-07-10 18:58:23 -0300

    fix/adicionado HttpContextAccessor

--- WebApi/Controllers/BaseController.cs
+++ WebApi/Controllers/BaseController.cs
@@ -1,11 +1,17 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Services.ApplicationServices.Interfaces;
 
 namespace HelloPets.WebApi.Controllers;
 
 [ApiController]
-[Authorize("")]
 public abstract class BaseController : ControllerBase
 {
-    
+    private readonly ITokenService _tokenService;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    protected BaseController(ITokenService tokenService, IHttpContextAccessor httpContextAccessor)
+    {
+        _tokenService = tokenService;
+        _httpContextAccessor = httpContextAccessor;
+    }
 }
