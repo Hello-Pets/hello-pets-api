8b5de7804b2073da08e8876a0cc2c2ab62c39e52
oGrandeK <kelvimkauam@gmail.com>
2024-07-14 20:02:21 -0300

    fix/refatorado UserController

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -24,45 +24,66 @@ public class UserController : BaseController
     [HttpPost("/v1/user")]
     [AllowAnonymous]
     public async Task<IActionResult> RegisterUser([FromBody]CreateUserViewModel tutor) {
-        // Validacoes basicas de email.
-        if(!tutor.Email.Equals(tutor.EmailVerification)) throw new ArgumentNullException("Email e verificação de email não coincidem");
-        if(string.IsNullOrWhiteSpace(tutor.Email) || string.IsNullOrWhiteSpace(tutor.EmailVerification)) throw new ArgumentException("Email inválido");
+    
+            // Validacoes basicas de email.
+            if(!tutor.Email.Equals(tutor.EmailVerification)) return BadRequest("Email e verificação de email não coincidem");
+            if(string.IsNullOrWhiteSpace(tutor.Email) || string.IsNullOrWhiteSpace(tutor.EmailVerification)) BadRequest("Email inválido");
+    
+            // Verifica se ja existe usuario com email fornecido.
+            var existingUser = await _tutorRepository.GetTutorByEmailAsync(tutor.Email);
+    
+            // Cria variavel para a senha hasheada.
+            string passwordHashed;
+    
+            // Lança excecao caso exista usuario com o mesmo email e com o mesmo tipo ativo.
+            if(existingUser != null && existingUser.IsActive && existingUser.UserType.Equals(tutor.UserType)) BadRequest("Email já cadastrado");
+    
+            // Caso usuario tenha escolhido o tipo da conta para Business cairá neste fluxo.
+            if(tutor.UserType.Equals(UserType.Business)) {
+                // Valida se o CNPJ informado tem 14 digitos
+                if(tutor.Document is null || tutor.Document.Length < 14 || tutor.Document.Length > 14) return BadRequest("CNPJ precisa conter 14 digitos");
+    
+                // Faz hash da senha informada.
+                passwordHashed = _passwordService.CreateHash(tutor.Password.Trim() + tutor.Salt.ToString());
+    
+                // Cria o novo User do tipo business.
+                var business = new Tutor{
+                    Email = tutor.Email, 
+                    Document = tutor.Document,
+                    DocumentType =DocumentType.CNPJ, 
+                    CreatedAt = DateTime.UtcNow, 
+                    IsActive = true, 
+                    PublicId = Guid.NewGuid(),
+                    Password = passwordHashed, 
+                    Salt = tutor.Salt.ToString(),
+                    UserType = UserType.Business
+                };
 
-        // Verifica se ja existe usuario com email fornecido.
-        var existingUser = await _tutorRepository.GetTutorByEmailAsync(tutor.Email);
-
-        // Cria variavel para a senha hasheada.
-        string passwordHashed;
-
-        // Lança excecao caso exista usuario com o mesmo email e com o mesmo tipo ativo.
-        if(existingUser != null && existingUser.IsActive && existingUser.UserType.Equals(tutor.UserType)) throw new Exception("Email já cadastrado");
-
-        // Caso usuario tenha escolhido o tipo da conta para Business cairá neste fluxo.
-        if(tutor.UserType.Equals(UserType.Business)) {
-            // Valida se o CNPJ informado tem 14 digitos
-            if(tutor.Document?.Length < 0 || tutor.Document?.Length > 14) throw new ArgumentException("CNPJ precisa conter 14 digitos");
-
+                // Salva o novo tutor no banco.
+                await _tutorRepository.CreateTutorAsync(business);
+    
+                return Ok();
+            }
+    
             // Faz hash da senha informada.
             passwordHashed = _passwordService.CreateHash(tutor.Password.Trim() + tutor.Salt.ToString());
+    
+            // Cria novo User do tipo Other.
+            var newTutor = new Tutor{
+                Email = tutor.Email, 
+                DocumentType =DocumentType.Other, 
+                CreatedAt = DateTime.UtcNow, 
+                IsActive = true, 
+                PublicId = Guid.NewGuid(),
+                Password = passwordHashed, 
+                Salt = tutor.Salt.ToString(),
+                UserType = UserType.Tutor
+            };
 
-            // Cria o novo User do tipo business.
-            var business = new Tutor(_tokenService.GetUserIdFromToken(), string.Empty, tutor.Document, tutor.Email, DocumentType.CNPJ, DateTime.UtcNow, default, true, Guid.NewGuid(), string.Empty, passwordHashed, tutor.Salt.ToString(), string.Empty, default, string.Empty, string.Empty, default, UserType.Business, new List<UserPets>(), new HelloPetsFile());
-
-            // Salva o novo tutor no banco.
-            await _tutorRepository.CreateTutorAsync(business);
-
-            return Ok();
-        }
-
-        // Faz hash da senha informada.
-        passwordHashed = _passwordService.CreateHash(tutor.Password.Trim() + tutor.Salt.ToString());
-
-        // Cria novo User do tipo Other.
-        var newTutor = new Tutor(_tokenService.GetUserIdFromToken(), string.Empty, string.Empty, tutor.Email, DocumentType.Other, DateTime.UtcNow, default, true, Guid.NewGuid(), string.Empty, passwordHashed, tutor.Salt.ToString(), string.Empty, default, string.Empty, string.Empty, default, UserType.Tutor, new List<UserPets>(), new HelloPetsFile());
-
-        //Salva o novo tutor no banco.
-        await _tutorRepository.CreateTutorAsync(newTutor);
-
-        return Ok(); 
+    
+            //Salva o novo tutor no banco.
+            await _tutorRepository.CreateTutorAsync(newTutor);
+    
+            return Ok(); 
     }
 }
