5a21d4a09f15724070ec84dad2f71092a448b239
oGrandeK <kelvimkauam@gmail.com>
2024-07-15 21:25:42 -0300

    fix/implementado geracao automatica de valores padroes

--- Data/Entities/Entity.cs
+++ Data/Entities/Entity.cs
@@ -8,16 +8,14 @@ public abstract class Entity
     public string? Name { get; set;}
     public string? Document { get; set; }
     public DocumentType? DocumentType { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
-    public bool IsActive { get; set; }
-    public Guid PublicId { get; set; }
+    public bool IsActive { get; set; } = true;
+    public Guid PublicId { get; set; } = Guid.NewGuid();
     public string? Bio { get; set; }
     public DateTime? Birthdate { get; set; }
     public int? ProfileImageId { get; set; }
     public virtual HelloPetsFile? File { get; set; }
-
-    protected Entity() => PublicId = Guid.NewGuid();
 
     public bool Equals(int id) => Id == id;
     public override int GetHashCode() => Id.GetHashCode();
--- Data/Entities/Tutor.cs
+++ Data/Entities/Tutor.cs
@@ -6,7 +6,7 @@ public class Tutor : Entity
     public string? Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
-    public string Salt { get; set; }
+    public string Salt { get; set; } = Guid.NewGuid().ToString();
     public string? Phone { get; set; }
     public string? Address { get; set; }
     public UserType UserType { get; set; }
