89288a404634f3ae9da10123b8ff0fd46fe7fae4
oGrandeK <kelvimkauam@gmail.com>
2024-07-17 16:56:07 -0300

    refactor/melhorada leitura dos codigos

--- Data/Repositories/TutorRepository.cs
+++ Data/Repositories/TutorRepository.cs
@@ -2,7 +2,6 @@ using HelloPets.Data.Context;
 using HelloPets.Data.Entities;
 using HelloPets.Data.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace HelloPets.Data.Repositories;
 
@@ -15,18 +14,23 @@ public class TutorRepository : ITutorRepository
         _context = context;
     }
 
-    public async Task<IEnumerable<Tutor>> GetTutorsAsync() => await _context.Tutors.Include(x => x.UserPets)
-        .Where(x => x.IsActive)
-        .ToListAsync();
+    public async Task<IEnumerable<Tutor>> GetTutorsAsync() => 
+        await _context.Tutors.Include(x => x.UserPets)
+            .Where(x => x.IsActive)
+            .ToListAsync();
 
-    public async Task<Tutor> GetTutorByIdAsync(int id) => await _context.Tutors.SingleOrDefaultAsync(x => x.Id == id && x.IsActive);
+    public async Task<Tutor> GetTutorByIdAsync(int id) => 
+        await _context.Tutors.SingleOrDefaultAsync(x => x.Id == id && x.IsActive);
 
-    public async Task<Tutor> GetTutorByPublicIdAsync(Guid publicId) => await _context.Tutors.SingleOrDefaultAsync(x => x.PublicId == publicId && x.IsActive);
+    public async Task<Tutor> GetTutorByPublicIdAsync(Guid publicId) => 
+        await _context.Tutors.SingleOrDefaultAsync(x => x.PublicId == publicId && x.IsActive);
 
-    public async Task<Tutor> GetTutorByDocumentAsync(string documentNumber) => await _context.Tutors.SingleOrDefaultAsync(x => x.Document == documentNumber && x.IsActive);
+    public async Task<Tutor> GetTutorByDocumentAsync(string documentNumber) => 
+        await _context.Tutors.SingleOrDefaultAsync(x => x.Document == documentNumber && x.IsActive);
 
-    public async Task<bool> IsRegistered(string email) => await _context.Tutors.AnyAsync(x => x.Email.ToLower() == email.ToLower() && x.IsActive == true);
-    // Melhorar esse metodo.
+    public async Task<bool> IsRegistered(string email) => 
+        await _context.Tutors.AnyAsync(x => x.Email.ToLower() == email.ToLower() && x.IsActive == true);
+        
     public async Task<Tutor> CreateTutorAsync(Tutor tutor)
     {
         var newTutor = await _context.Tutors.AddAsync(tutor);
--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -26,26 +26,21 @@ public class UserController : BaseController
     [AllowAnonymous]
     public async Task<IActionResult> RegisterUser([FromBody]CreateUserViewModel userVM) 
     {
-            // Verifica se ja existe usuario com email fornecido e retorna BadRequest caso verdadeiro.
             if(await _tutorRepository.IsRegistered(userVM.Email))
                 return BadRequest("Email já cadastrado");
 
-            // Verifica se as senhas coincidem.
             if(!userVM.Password.Trim().Equals(userVM.PasswordVerification.Trim()))
                 return BadRequest("Senhas não coincidem");
     
-            // Valida CNPJ caso usuário tenha escolhido tipo Business.
             if(userVM.UserType.Equals(UserType.Business)) 
             {
-                // Valida se o CNPJ informado tem 14 digitos.
                 if(userVM.DocumentType == DocumentType.CNPJ &&
+                    userVM.Document is not null &&
                     userVM.Document?.Length != 14 && 
-                    userVM.Document.Any(ch => char.IsDigit(ch)) &&
-                    userVM.Document is not null)
-                    return BadRequest("CNPJ deve conter 14 digitos");
+                    userVM.Document.Any(ch => char.IsDigit(ch)))
+                        return BadRequest("CNPJ deve conter 14 digitos");
             }
 
-            // Instancia novo TUTOR com base nos dados inseridos no body da request.
             var user = new Tutor 
             {
                 Name = userVM.Name,
@@ -56,10 +51,8 @@ public class UserController : BaseController
                 Salt = userVM.Salt.ToString(),
             };
     
-            //Salva o novo TUTOR no banco.
             await _tutorRepository.CreateTutorAsync(user);
     
-            // Retorna view model com os dados necessarios.
             return Ok(new ReturnUserViewModel
             {
                 PublicId = user.PublicId.ToString().ToLower(),
@@ -72,25 +65,21 @@ public class UserController : BaseController
 
     [HttpPatch("user/{publicId}")]
     [AllowAnonymous]
-    public async Task<IActionResult> UpdateUser([FromRoute]Guid publicId, [FromBody] PatchUserViewModel user) {
-        // Busca usuario pela PublicId.
+    public async Task<IActionResult> UpdateUser([FromRoute]Guid publicId, [FromBody] PatchUserViewModel user) 
+    {
         var existingUser = await _tutorRepository.GetTutorByPublicIdAsync(publicId);
 
-        // Retorna BadRequest caso usuário informado nao exista ou esteja inativo.
         if(existingUser is null) return BadRequest("Usuário não existe com Id informado.");
 
         if(existingUser.Id != user.Id)
             return BadRequest("ID do usuário diferente do fornecido");
 
-        // Verifica se o campo Bio informado é diferente do existente no banco, caso sim altera.
         if(existingUser.Bio != user.Bio)
             existingUser.Bio = user.Bio;
 
-        // Verifica se o campo Address informado é diferente do existente no banco, caso sim altera.
         if(existingUser.Address != user.Address) 
             existingUser.Address = user.Address;
 
-        // Verifica se o campo ProfileImageId informado é diferente do existente no banco, caso sim altera.
         if(existingUser.ProfileImageId != user.ProfileImageId) 
             existingUser.ProfileImageId = user.ProfileImageId;
 
@@ -102,18 +91,14 @@ public class UserController : BaseController
     [HttpDelete("user/{publicId}")]
     public async Task<IActionResult> DeleteUser([FromRoute] Guid publicId, [FromBody] DeleteUserViewModel userVM) 
     {
-        // Busca usuario pelo PublicId informado.
         var user = await _tutorRepository.GetTutorByPublicIdAsync(publicId);
 
-        // Retorna BadRequest caso usuario não exista na base de dados.
         if(user is null)
             return BadRequest("Usuário não existente.");
 
-        // Retorna BadRequest caso ID seja diferente do fornecido pelo usuario no body.
         if(user.Id != userVM.Id)
             return BadRequest("ID do usuário diferente do fornecido.");
 
-        // Acessa o metodo DeleteTutor do repositorio ja marcando usuario como INATIVO e atualizando o UPDATEAT.
         await _tutorRepository.DeleteTutorAsync(user);
 
         return NoContent();
