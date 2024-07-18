10f90538d3a8d6983e89f481b065ddc03d88253c
Andershowrj <anderson92martins@gmail.com>
2024-07-18 22:36:34 +0100

    Adicionada LoginViewModel

---
+++ WebApi/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email obrigatorio")]
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatoria")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
