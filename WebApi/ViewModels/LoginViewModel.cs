1344326bb05179979a107cbe4afaeeeeb257e8ba
Andershowrj <anderson92martins@gmail.com>
2024-07-20 20:36:36 +0100

    Removida propriedade desnecessaria

--- WebApi/ViewModels/LoginViewModel.cs
+++ WebApi/ViewModels/LoginViewModel.cs
@@ -11,7 +11,5 @@ namespace WebApi.ViewModels
         [Required(ErrorMessage = "Senha obrigatoria")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
-        public bool RememberMe { get; set; }
     }
 }
