1b434e6a8c30469fb18e31406e1cbbd2e70c1407
Andershowrj <anderson92martins@gmail.com>
2024-07-24 22:16:32 +0100

    criada updatepasswordviewmodel

---
+++ WebApi/ViewModels/UpdatePasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.ViewModels
+{
+    public record UpdatePasswordViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MinLength(6, ErrorMessage = "O campo {0} deve conter no mínimo 6 caracteres.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MinLength(6, ErrorMessage = "O campo {0} deve conter no mínimo 6 caracteres.")]
+        public string PasswordVerification { get; set; }
+    }
+}
