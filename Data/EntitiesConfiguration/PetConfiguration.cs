52e298d1f603cbc2641d9492928c6020474ba220
oGrandeK <kelvimkauam@gmail.com>
2024-07-29 22:17:58 -0300

    refactor/removido mapeamentos desnecessarios

--- Data/EntitiesConfiguration/PetConfiguration.cs
+++ Data/EntitiesConfiguration/PetConfiguration.cs
@@ -8,55 +8,17 @@ public class PetConfiguration : IEntityTypeConfiguration<Pet>
 {
     public void Configure(EntityTypeBuilder<Pet> builder)
     {
-        builder.HasKey(x => x.Id);
-
         builder.HasIndex(x => x.PublicId);
 
-        builder.Property(x => x.Name).HasMaxLength(30).IsRequired(true);
+        builder.Property(x => x.Name).HasMaxLength(30).IsRequired(false);
 
         builder.Property(x => x.Document).HasMaxLength(40).IsRequired(false);
 
-        builder.Property(x => x.DocumentType);
-
-        builder.Property(x => x.CreatedAt);
-
-        builder.Property(x => x.UpdatedAt);
-
         builder.Property(x => x.IsActive).HasDefaultValue(true);
-
-        builder.Property(x => x.PublicId).IsRequired(true);
 
         builder.Property(x => x.Bio).HasMaxLength(255).IsRequired(false);
 
-        builder.Property(x => x.Birthdate);
-
-        builder.OwnsOne(x => x.File).Property(x => x.Id);
-
-        builder.OwnsOne(x => x.File).Property(x => x.Name).HasMaxLength(100).IsRequired(false);
-
-        builder.OwnsOne(x => x.File).Property(x => x.Type);
-
-        builder.OwnsOne(x => x.File).Property(x => x.PublicLink).HasMaxLength(255).IsRequired(false);
-
         builder.Property(x => x.Furcolor).HasMaxLength(20).IsRequired(false);
-
-        builder.Property(x => x.Neutered).HasDefaultValue(false);
-
-        builder.Property(x => x.HasMicroChip).HasDefaultValue(false);
-
-        builder.Property(x => x.Size);
-
-        builder.OwnsOne(x => x.Breed).Property(x => x.Id);
-
-        builder.OwnsOne(x => x.Breed).Property(x => x.Name).HasMaxLength(20).IsRequired(false);
-
-        builder.OwnsOne(x => x.Breed).OwnsOne(x => x.Specie).Property(x => x.Id);
-
-        builder.OwnsOne(x => x.Breed).OwnsOne(x => x.Specie).Property(x => x.Name).HasMaxLength(30).IsRequired(false);
-
-        builder.OwnsOne(x => x.Breed).OwnsOne(x => x.Specie).Property(x => x.IsActive).HasDefaultValue(true);
-
-        builder.OwnsOne(x => x.Breed).Property(x => x.IsActive).HasDefaultValue(true);
 
         builder.HasMany(x => x.UserPets).WithOne(x => x.Pet);
     }
--- Data/EntitiesConfiguration/UserConfiguration.cs
+++ Data/EntitiesConfiguration/UserConfiguration.cs
@@ -8,51 +8,21 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        builder.HasKey(x => x.Id);
-
         builder.HasIndex(x => x.Email).IsUnique();
 
         builder.HasIndex(x => x.PublicId).IsUnique();
 
-        builder.Property(x => x.Name).HasMaxLength(121).IsRequired(true);
-
-        builder.Property(x => x.Document).HasMaxLength(40).IsRequired(false);
-
-        builder.Property(x => x.DocumentType);
-
-        builder.Property(x => x.CreatedAt);
-
-        builder.Property(x => x.UpdatedAt);
+        builder.Property(x => x.Document).HasMaxLength(32).IsRequired(false);
 
         builder.Property(x => x.IsActive).HasDefaultValue(true);
 
-        builder.Property(x => x.PublicId).IsRequired(true);
-
         builder.Property(x => x.Bio).HasMaxLength(255).IsRequired(false);
 
-        builder.Property(x => x.Birthdate);
+        builder.Property(x => x.Username).HasMaxLength(64).IsRequired(false);
 
-        builder.OwnsOne(x => x.File).Property(x => x.Id);
+        builder.Property(x => x.Phone).HasMaxLength(32).IsRequired(false);
 
-        builder.OwnsOne(x => x.File).Property(x => x.Name).HasMaxLength(100).IsRequired(false);
-
-        builder.OwnsOne(x => x.File).Property(x => x.Type);
-
-        builder.OwnsOne(x => x.File).Property(x => x.PublicLink).HasMaxLength(255).IsRequired(false);
-
-        builder.Property(x => x.Username).HasMaxLength(60).IsRequired(false);
-
-        builder.Property(x => x.Password).IsRequired(true);
-
-        builder.Property(x => x.Email).IsRequired(true);
-
-        builder.Property(x => x.Salt).IsRequired(true);
-
-        builder.Property(x => x.Phone).HasMaxLength(20).IsRequired(false);
-
-        builder.Property(x => x.Address).HasMaxLength(120).IsRequired(false);
-
-        builder.Property(x => x.UserType);
+        builder.Property(x => x.Address).HasMaxLength(122).IsRequired(false);
 
         builder.HasMany(x => x.UserPets).WithOne(x => x.User);
     }
